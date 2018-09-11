d39cc8b62c95ec92de88c88d7c5861d6d1952b29
Trezy <tre@trezy.com>
2018-09-11 13:44:54 -0500

    I made some changes and stuff

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // Module imports
+import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 
 
@@ -24,9 +25,19 @@ export class Wizard extends Component {
     Local Properties
   \***************************************************************************/
 
+  static defaultProps = {
+    defaultStep: null,
+    onStepChange: () => {},
+  }
+
+  static propTypes = {
+    defaultStep: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    onStepChange: PropTypes.func,
+  }
+
   /* eslint-disable react/no-unused-state */
   state = {
-    step: 0,
+    currentStep: 0,
     wizardState: {},
     setStep: this.setStep,
   }
@@ -46,22 +57,24 @@ export class Wizard extends Component {
     } = this.props
 
     this.setState(state => ({
-      step: stepId,
+      currentStep: stepId,
       wizardState: {
         ...state.wizardState,
         ...newState,
       },
-    }), () => {
+    }), reallyNewState => {
       if (onStepChange) {
-        onStepChange(this.state.wizardState)
+        onStepChange(reallyNewState.wizardState)
       }
     })
   }
 
   render () {
+    const { children } = this.props
+
     return (
       <Provider value={this.state}>
-        {this.props.children}
+        {children}
       </Provider>
     )
   }
@@ -71,13 +84,65 @@ export class Wizard extends Component {
 
 
 
-export const Step = ({ stepId, children }) => (
-  <Consumer>
-    {({ step, ...childProps }) => {
-      if (step === stepId) {
-        return children(childProps)
+export class Step extends Component {
+  /***************************************************************************\
+    Local Properties
+  \***************************************************************************/
+
+  static defaultProps = {
+    render: null,
+  }
+
+  static propTypes = {
+    id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    render: PropTypes.func,
+  }
+
+
+
+
+
+  /***************************************************************************\
+    Private Methods
+  \***************************************************************************/
+
+  _renderChildren (childProps) {
+    const {
+      children,
+      render,
+    } = this.props
+
+    return React.Children.map(children, child => {
+      if (typeof child === 'function') {
+        return child(childProps)
       }
-      return null
-    }}
-  </Consumer>
-)
+
+      if (render) {
+        return render(childProps)
+      }
+
+      return React.cloneElement(child, { ...childProps })
+    })
+  }
+
+
+
+
+
+  /***************************************************************************\
+    Public Methods
+  \***************************************************************************/
+  render () {
+    const { id } = this.props
+
+    return (
+      <Consumer>
+        {({ currentStep, ...childProps }) => (
+          <div hidden={currentStep !== id}>
+            {this._renderChildren(childProps)}
+          </div>
+        )}
+      </Consumer>
+    )
+  }
+}
